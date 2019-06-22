2257b7d70cf1c9709c02ec5b18a285e4b015daa3
Roman165 <roman16545@gmail.com>
2019-06-22 19:37:20 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+(function () {
+	'use strict';
+
+			var playList = [
+ {
+  author: "LED ZEPPELIN",
+  song:"STAIRWAY TO HEAVEN"
+ },
+ {
+  author: "QUEEN",
+  song:"BOHEMIAN RHAPSODY"
+ },
+ {
+  author: "LYNYRD SKYNYRD",
+  song:"FREE BIRD"
+ },
+ {
+  author: "DEEP PURPLE",
+  song:"SMOKE ON THE WATER"
+ },
+ {
+  author: "JIMI HENDRIX",
+  song:"ALL ALONG THE WATCHTOWER"
+ },
+ {
+  author: "AC/DC",
+  song:"BACK IN BLACK"
+ },
+ {
+  author: "QUEEN",
+  song:"WE WILL ROCK YOU"
+ },
+ {
+  author: "METALLICA",
+  song:"ENTER SANDMAN"
+ }
+];
+	function myfunc(autor, song){
+		var list = document.getElementById("list");
+
+		for (var i = 0; i< playList.length; i++) {
+
+   		var listItem = document.createElement("li"),
+   			firstItem = i+1;
+   			
+   		listItem.innerHTML = '<span>' + firstItem + '</span>' + playList[i].song;
+  		list.appendChild(listItem);
+		}
+}
+  myfunc()
+
+})();
